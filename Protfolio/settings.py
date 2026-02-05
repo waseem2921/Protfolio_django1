a97c53bfb6dd31fc67663677ac81b5011de63c6d
waseem2921 <waseemferozkhan2921@gmail.com>
2026-02-05 11:17:32 +0530

    SQL Config

--- Protfolio/settings.py
+++ Protfolio/settings.py
@@ -73,11 +73,15 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME", "postgres"),
+        "USER": os.getenv("DB_USER", "postgres"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "2921"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": "5432",
     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
