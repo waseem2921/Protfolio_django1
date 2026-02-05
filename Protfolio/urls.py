c2cf9aaa919cd155beff3f6d26a76a54291d8ef6
waseem2921 <waseemferozkhan2921@gmail.com>
2026-02-05 11:44:05 +0530

    MEDIA URL CHANGE

--- Protfolio/urls.py
+++ Protfolio/urls.py
@@ -28,3 +28,8 @@ urlpatterns += static(
     settings.STATIC_URL,
     document_root=settings.STATIC_ROOT
 )
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
