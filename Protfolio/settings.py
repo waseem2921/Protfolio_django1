236f44013aaa022d048b00b9b772b9b1ce7adf47
waseem2921 <waseemferozkhan2921@gmail.com>
2026-02-06 12:46:23 +0530

    init

--- Protfolio/settings.py
+++ Protfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('django-insecure-c@gv!lmj7-=d&qx)4lr5jj@xky+#tj-eny0+=pc-e#_+_-6nx7')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
